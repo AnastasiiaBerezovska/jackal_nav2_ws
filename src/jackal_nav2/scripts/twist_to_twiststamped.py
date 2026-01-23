004a50e6a3fc028b347d4a39c4ca6ac73d6f5bab
quattro <jesse.quattrociocchi@parsons.us>
2026-01-23 18:04:13 -0500

    nav2 waypoint navigation

--- src/jackal_nav2/scripts/twist_to_twiststamped.py
+++ src/jackal_nav2/scripts/twist_to_twiststamped.py
@@ -3,17 +3,21 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, TwistStamped
 
-class TwistToTwistStamped(Node):
+class TwistToTwistStamped(Node): # this called node literally 
     def __init__(self):
         super().__init__('twist_to_twiststamped')
         self.subscription = self.create_subscription(
             Twist,
             '/cmd_vel_nav',
+            #   '/cmd_vel_smoothed',
             self.twist_callback,
             10)
-        self.publisher = self.create_publisher(
+        self.publisher = self.create_publisher( # change later to /lonebot/cmd_vel
             TwistStamped,
-            '/lonebot/joy_teleop/cmd_vel',
+             "/lonebot/platform/cmd_vel",
+            # '/lonebot/joy_teleop/cmd_vel',
+            # '/lonebot/bt_teleop/cmd_vel',
+            # '/lonebot/cmd_vel',
             10)
         self.get_logger().info('Twist to TwistStamped converter started!')
 
